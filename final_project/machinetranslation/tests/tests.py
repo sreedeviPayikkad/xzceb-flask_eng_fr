698119ce211380299b3ffff7259e390c5ac79e68
sreedeviPayikkad <sreedevi.payikkad@gmail.com>
2022-01-19 22:55:56 -0800

    updated unittests

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -1,9 +1,12 @@
 import unittest
-from machinetranslation import english_to_french, french_to_english
+from translator import english_to_french, french_to_english
 
 class TestFrenchToEnglish(unittest.TestCase):
     def test_for_valid_input(self):
         self.assertEqual(french_to_english('Bonjour'), 'Hello')
+
+    def test_for_negative_result(self):
+        self.assertNotEqual(french_to_english('Bonjour'), 'Bonjour')
 
     def test_for_null(self) :
         self.assertEqual(french_to_english(), 'Invalid input')
@@ -11,6 +14,9 @@ class TestFrenchToEnglish(unittest.TestCase):
 class TestEnglishToFrench(unittest.TestCase):
     def test_for_valid_input(self):
         self.assertEqual(english_to_french('Hello'), 'Bonjour')
+
+    def test_for_negative_result(self):
+        self.assertNotEqual(english_to_french('Hello'), 'Hello')
 
     def test_for_null(self) :
         self.assertEqual(english_to_french(), 'Invalid input')
